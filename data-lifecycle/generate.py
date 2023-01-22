e054c1cdf82221803d43c38a6677e517a87512e4
hellmarbecker <becker@hellmar-becker.de>
2023-01-22 12:15:49 +0100

    Add date logic

--- data-lifecycle/generate.py
+++ data-lifecycle/generate.py
@@ -2,6 +2,7 @@ import random
 from datetime import date, timedelta
 
 MAXUSERS = 10000
+ROWSPERDAY = 10
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
@@ -10,9 +11,11 @@ def daterange(start_date, end_date):
 def main():
 
     start_date = date(2022, 1, 1)
-    end_date = date(2022, 2, 1)
+    end_date = date(2022, 6, 1)
     for single_date in daterange(start_date, end_date):
-        print(single_date.strftime("%Y-%m-%d") + ",u" + "{:04d}".format(random.randrange(MAXUSERS)))
+        this_day = single_date.strftime("%Y-%m-%d")
+        for i in range(ROWSPERDAY):
+            print(this_day + ",u" + "{:04d}".format(random.randrange(MAXUSERS)))
 
 
 if __name__ == "__main__":
