5f27b2c13494002c5177daf4c578b8c75bc1412d
hellmarbecker <becker@hellmar-becker.de>
2023-02-26 13:40:21 +0100

    Refactor

--- schemaless/generate.py
+++ schemaless/generate.py
@@ -6,27 +6,29 @@ import sys
 
 ROWSPERSEC = 10
 NUMSCHEMA = 10
+STEPUP = 5
 
-dim_list = ['d' + str(i) for i in range(10)] + ['m' + str(i) for i in range(10)] + ['v' + str(i) for i in range(10)]
 dim_types = { 'd': ['word'], 'm': ['pyfloat'], 'v': ['pyint', 'word', 'pyfloat'] }
+
+fake = Faker()
 
 def datetimerange(start_date):
     for n in range(7 * 24 * 60 * 60): # 1 week, 1 record per second
         yield start_date + timedelta(seconds=n)
 
-
-def main():
-
-    fake = Faker()
-    fake.set_arguments('w', {'nb':2})
-
+def genspec(n):
+    dim_list = ['d' + str(i) for i in range(n)] + ['m' + str(i) for i in range(n)] + ['v' + str(i) for i in range(n)]
     spec = []
     for n in range(NUMSCHEMA):
         fields = fake.words(nb = 5, ext_word_list = dim_list) 
         spec.append({ f : fake.word(ext_word_list = dim_types[f[0]]) for f in fields })
+    return spec
 
-    # print(spec)
-    # sys.exit(0)
+def main():
+
+    rows = 0
+    ndims = 10
+    spec = genspec(ndims)
 
     start_date = datetime(2022, 1, 1)
     for ts in datetimerange(start_date):
@@ -35,6 +37,10 @@ def main():
             obj = json.loads(fake.json(data_columns=spec[fake.random_int(0, NUMSCHEMA-1)], num_rows=1))
             row = dict([("ts", this_dt)]) | obj
             print(json.dumps(row))
+            rows += 1
+            if (rows % STEPUP == 0):
+                ndims += 10
+                spec = genspec(ndims)
 
 
 if __name__ == "__main__":
