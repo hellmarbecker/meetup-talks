de909f48ab12d519b5d9c6977504c8764577af27
hellmarbecker <becker@hellmar-becker.de>
2023-02-26 11:06:28 +0100

    Add dimension and measure fields

--- schemaless/generate.py
+++ schemaless/generate.py
@@ -2,11 +2,13 @@ import math, random
 import json
 from datetime import date, datetime, timedelta
 from faker import Faker
+import sys
 
-ROWSPERSEC = 1000
+ROWSPERSEC = 10
 NUMSCHEMA = 10
 
-dim_list = ['dim' + str(i) for i in range(10)]
+dim_list = ['d' + str(i) for i in range(10)] + ['m' + str(i) for i in range(10)] + ['v' + str(i) for i in range(10)]
+dim_types = { 'd': ['word'], 'm': ['pyfloat'], 'v': ['pyint', 'word', 'pyfloat'] }
 
 def datetimerange(start_date):
     for n in range(7 * 24 * 60 * 60): # 1 week, 1 record per second
@@ -21,7 +23,10 @@ def main():
     spec = []
     for n in range(NUMSCHEMA):
         fields = fake.words(nb = 5, ext_word_list = dim_list) 
-        spec.append({ f : fake.word(ext_word_list = ['pyint', 'word', 'pyfloat']) for f in fields })
+        spec.append({ f : fake.word(ext_word_list = dim_types[f[0]]) for f in fields })
+
+    # print(spec)
+    # sys.exit(0)
 
     start_date = datetime(2022, 1, 1)
     for ts in datetimerange(start_date):
