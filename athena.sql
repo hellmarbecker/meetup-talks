c8faf02594f2addbeac06b6c6185bd8fdf4f4862
Hellmar Becker <becker@hellmar-becker.de>
2023-08-15 10:21:57 +0200

    Delete athena.sql

--- athena.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE testdata.testdata (
-  ts  TIMESTAMP,
-  key STRING,
-  val DOUBLE
-)
-PARTITIONED BY ( month(ts) )
-LOCATION 's3://hbecker-us-east1/testdata/'
-TBLPROPERTIES ( 'table_type'='ICEBERG' );
