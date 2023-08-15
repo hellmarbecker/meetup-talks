f857bd2556f9c70ef9c56c4bfda33333953181ac
Hellmar Becker <becker@hellmar-becker.de>
2023-08-15 10:47:36 +0200

    Update athena.sql

--- iceberg-ext/athena.sql
+++ iceberg-ext/athena.sql
@@ -9,3 +9,6 @@ CREATE TABLE iceberg_test.iceberg_testdata (
 PARTITIONED BY ( month(ts) )
 LOCATION 's3://hbecker-us-east-1/iceberg_testdata/'
 TBLPROPERTIES ( 'table_type'='ICEBERG' );
+
+
+INSERT INTO iceberg_test.iceberg_testdata VALUES (TIMESTAMP'2023-08-15', 'a1', 23.9);
