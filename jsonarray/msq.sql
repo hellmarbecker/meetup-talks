b29121cb82c273fba732ee808f2f72d4287a1634
Hellmar Becker <becker@hellmar-becker.de>
2023-12-17 13:40:49 +0100

    New Druid 29 function

---
+++ jsonarray/msq.sql
@@ -0,0 +1,21 @@
+REPLACE INTO "jsonarray" OVERWRITE ALL
+WITH "ext" AS (
+  SELECT *
+  FROM TABLE(
+    EXTERN(
+      '{"type":"local","baseDir":"/Users/hellmarbecker/meetup-talks/jsonarray","filter":"*json"}',
+      '{"type":"json"}'
+    )
+  ) EXTEND ("id" BIGINT, "shop" VARCHAR, "name" VARCHAR, "phoneNumber" VARCHAR, "address" VARCHAR, "pizzas" TYPE('COMPLEX<json>'), "timestamp" BIGINT)
+)
+SELECT
+  MILLIS_TO_TIMESTAMP("timestamp") AS "__time",
+  "id",
+  "shop",
+  "name",
+  "phoneNumber",
+  "address",
+  JSON_VALUE(p, '$.pizzaName') AS pizzaName,
+  JSON_QUERY_ARRAY(p, '$.additionalToppings') AS additionalToppings
+FROM "ext" CROSS JOIN UNNEST(JSON_QUERY_ARRAY(pizzas, '$')) AS lineitems(p)
+PARTITIONED BY DAY
